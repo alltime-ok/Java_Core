147c9c4de7ee2ed3d6b46b6b17608aa854da30a0
alltime-ok <igor.duda6666@gmail.com>
2024-01-27 23:49:23 +0200

    new class Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello Ihor!!!!");
+    }
+
+    public class Test{
+        public int a;
+        public String b;
     }
 }
