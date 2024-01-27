62a05dbd8bc87b0b2feefeda455632e8e205366d
alltime-ok <igor.duda6666@gmail.com>
2024-01-28 00:38:08 +0200

    commit master name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Ihor!!!!");
+        System.out.println("Hello Ihor Duda!!!!");
     }
 
     public class Test{
